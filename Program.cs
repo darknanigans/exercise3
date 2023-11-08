ffecae9e7dc8c2fb18f4e43325174d3ed30d0727
darknanigans <Naura.Khan@s.capregboces.org>
2023-11-08 12:46:13 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace exercise3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          ///set age
+            const int BkAge = 16;
+
+        ///print age
+        Console.WriteLine("Age: " + BkAge);
+        }
+    }
+}
